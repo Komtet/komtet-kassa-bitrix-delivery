ea2ca8d9793698e4a76b081c84337867848cf70c
alexandr.motmom@gmail.com <alexandr.motmom@gmail.com>
2019-05-23 11:21:49 +0300

    курьер - необязательная настройка

--- komtet.delivery/classes/general/KomtetDelivery.php
+++ komtet.delivery/classes/general/KomtetDelivery.php
@@ -1,10 +1,11 @@
 <?php
 
+use Bitrix\Main\UserTable;
 use Komtet\KassaSdk\Exception\SdkException;
 use Komtet\KassaSdk\Client;
+use Komtet\KassaSdk\OrderManager;
 use Komtet\KassaSdk\TaxSystem;
 use Komtet\KassaSdk\Vat;
-use Bitrix\Main\UserTable;
 
 
 class KomtetDelivery
@@ -52,4 +53,29 @@ class KomtetDelivery
 class KomtetDeliveryBase
 {
     protected $manager;
+    protected $shouldForm;
+    protected $taxSystem;
+
+    public function __constructor()
+    {
+        $options = $this->getOptions();
+        $client = new Client($options['key'], $options['secret']);
+
+        $this->manager = new OrderManager($client);
+        $this->shouldForm = $options['should_form'];
+        $this->taxSystem = $options['tax_system'];
+        $this->defaultCourier = $options['default_courier'];
+    }
+
+    public function getoptions()
+    {
+        $moduleID = 'komtet.delivery';
+        $result = array(
+            'key' => COption::GetOptionString($moduleID, 'shop_id'),
+            'secret' => COption::GetOptionString($moduleID, 'secret_key'),
+            'should_form' => COption::GetOptionInt($moduleID, 'should_form') == 1,
+            'tax_system' => intval(COption::GetOptionInt($moduleID, 'tax_system')),
+            'default_courier' => intval(COption::GetOptionInt($moduleID, 'default_courier'))
+        );
+    }
 }
