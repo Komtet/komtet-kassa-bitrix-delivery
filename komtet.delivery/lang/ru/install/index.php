18239b7b367f515cc992e73a0ffc549ce390abf5
alexandr.motmom@gmail.com <alexandr.motmom@gmail.com>
2019-05-30 14:19:11 +0300

    установка доп полей

--- komtet.delivery/lang/ru/install/index.php
+++ komtet.delivery/lang/ru/install/index.php
@@ -5,3 +5,7 @@ $MESS['KOMTETDELIVERY_MODULE_DESCRIPTION'] = 'Интеграция с серви
 
 $MESS['INSTALL_KOMTETDELIVERY'] = 'Модуль КОМТЕТ Касса Доставка установлен';
 $MESS['UNINSTALL_KOMTETDELIVERY'] = 'Модуль КОМТЕТ Касса Доставка удален';
+
+$MESS['MOD_ERR_SALE_NOT_FOUND'] = 'Модуль Sale не найден';
+$MESS['MOD_ERR_PERSON_NOT_FOUND'] = 'Типы плательщиков не найдены';
+$MESS['MOD_GROUP_NAME'] = 'КОМТЕТ Касса Доставка';
