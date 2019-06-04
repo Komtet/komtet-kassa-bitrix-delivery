99518cc09ceb5794e7b260484fca98a272c903a8
alexandr.motmom@gmail.com <alexandr.motmom@gmail.com>
2019-06-04 13:58:50 +0300

    валидация полей

--- komtet.delivery/classes/general/KomtetDelivery.php
+++ komtet.delivery/classes/general/KomtetDelivery.php
@@ -44,6 +44,12 @@ class KomtetDeliveryD7
     public function __construct()
     {
         $options = $this->getOptions();
+
+        if (!$this->optionsValidate($options)) {
+            error_log("Ошибка валидации настроек");
+            return false;
+        }
+
         $client = new Client($options['key'], $options['secret']);
 
         $this->manager = new OrderManager($client);
@@ -70,20 +76,23 @@ class KomtetDeliveryD7
             'order_status' => COption::GetOptionString($moduleID, 'order_status'),
             'delivery_status' => COption::GetOptionString($moduleID, 'delivery_status')
         );
-        foreach (array('key', 'secret', 'tax_system') as $key) {
-            if (empty($result[$key])) {
-                error_log(sprintf('Option "%s" for module "komtet.delivery" is required', $key));
-            }
-        }
+
         return $result;
     }
 
     public function createOrder($orderId)
     {
-        //обрубать если флаг не установлен
-        $order = OrderTable::load($orderId);
-        $customFields = CSaleOrderPropsValue::GetOrderProps($orderId);
+        if (!$this->shouldForm ) {
+            error_log(sprintf('[Order - %s] Заказ не создан, флаг генерации не установлен', $orderId));
+            return false;
+        }
 
+        $order = OrderTable::load($orderId);
+        if (!$order->isAllowDelivery()) {
+            return false;
+        }
+
+        $customFields = CSaleOrderPropsValue::GetOrderProps($orderId);
         while ($customField = $customFields->Fetch())
         {
             if ($customField['GROUP_NAME'] === $this->modGroupName)
@@ -91,13 +100,19 @@ class KomtetDeliveryD7
                 $customFieldList[$customField["CODE"]] = $customField["VALUE"];
             }
         }
-        foreach (array('kkd_address', 'kkd_date', 'kkd_time_start', 'kkd_time_end') as $key) {
-            if (empty($customFieldList[$key])) {
-                error_log(sprintf('Custom field "%s" for module "komtet.delivery" is required', $key));
-            }
-        }
-        $userId = $order->getUserId();
+
+        $userId = $order->getPersonTypeId();
         $rsUser = UserTable::getById($userId)->fetch();
+
+        if (!$this->customFieldsValidate($customFieldList)) {
+            error_log(sprintf('[Order - %s] Ошибка заполенния дополнительных полей', $orderId));
+            return false;
+        }
+
+        if (!$this->userValidate($rsUser)) {
+            error_log(sprintf('[Order - %s] Ошибка валидации пользователя', $orderId));
+            return false;
+        }
 
         $orderDelivery = new Order($order->getId(), 'new', $this->taxSystem, $order->isPaid());
         $orderDelivery->setClient($customFieldList['kkd_address'],
@@ -109,24 +124,39 @@ class KomtetDeliveryD7
                                           $rsUser['LAST_NAME']));
 
         $positions = $order->getBasket();
-
         foreach ($positions as $position) {
             if ($this->taxSystem == TaxSystem::COMMON) {
-                $itemVatRate = round(floatval($position->getField('VAT_RATE')) * 100, 2);
+                $itemVatRate = strval(round(floatval($position->getField('VAT_RATE')) * 100, 2));
             } else {
                 $itemVatRate = Vat::RATE_NO;
             }
+
             $orderPosition = new OrderPosition(['oid' => $position->getField('ID'),
                                                 'name' => $position->getField('NAME'),
                                                 'price' => round($position->getPrice(),2),
                                                 'quantity' => $position->getQuantity(),
                                                 'total'=> round($position->getFinalPrice(),2),
-                                                'vat' => Vat::RATE_NO, //$itemVatRate,
+                                                'vat' => $itemVatRate,
                                                 'measure_name' => $position->getField('MEASURE_NAME'),
                                                ]);
 
             $orderDelivery->addPosition($orderPosition);
         }
+
+        if (!empty($order->getDeliveryIdList()))
+        {
+            $orderPosition = new OrderPosition(['oid' => $order->getDeliveryIdList()[0],
+                                                'name' => "Доставка",
+                                                'price' => round($order->getField("PRICE_DELIVERY"),2),
+                                                'quantity' => 1,
+                                                'total'=> round($order->getField("PRICE_DELIVERY"),2),
+                                                'vat' => Vat::RATE_NO,
+                                                'measure_name' => "шт",
+                                               ]);
+            $orderDelivery->addPosition($orderPosition);
+        }
+
+
         if (!$this->defaultCourier == 0) {
             $orderDelivery->setCourierId($this->defaultCourier);
         }
@@ -153,25 +183,55 @@ class KomtetDeliveryD7
 
     public function doneOrder($orderId)
     {
-        if(CModule::IncludeModule("sale"))
+        if(!CModule::IncludeModule("sale"))
         {
-          $order = OrderTable::load($orderId);
-          try {
-              CSaleOrder::PayOrder($orderId, $this->payStatus);
-              CSaleOrder::StatusOrder($orderId, $this->orderStatus);
-              $order->setField("ADDITIONAL_INFO", Date(CDatabase::DateFormatToPHP(CLang::GetDateFormat("FULL", LANG))));
-          } catch (Exception $e) {
-              echo($e->getMessage());
-          }
-          $shipments = $order->getShipmentCollection();
-          foreach ($shipments as $shipment)
-          {
-              if(!$shipment->isSystem())
-              {
-                  $shipment->setField('STATUS_ID', $this->deliveryStatus);
-              }
-          }
-          $order->save();
+            return false;
         }
+        $order = OrderTable::load($orderId);
+        try {
+            CSaleOrder::PayOrder($orderId, $this->payStatus);
+            CSaleOrder::StatusOrder($orderId, $this->orderStatus);
+            $order->setField("ADDITIONAL_INFO", Date(CDatabase::DateFormatToPHP(CLang::GetDateFormat("FULL", LANG))));
+        } catch (Exception $e) {
+            echo($e->getMessage());
+        }
+        $shipments = $order->getShipmentCollection();
+        foreach ($shipments as $shipment)
+        {
+            if(!$shipment->isSystem())
+            {
+                $shipment->setField('STATUS_ID', $this->deliveryStatus);
+            }
+        }
+        $order->save();
+
+    }
+
+    private function customFieldsValidate($customFieldList) {
+      foreach (array('kkd_address', 'kkd_date', 'kkd_time_start', 'kkd_time_end') as $key) {
+          if (empty($customFieldList[$key])) {
+              error_log(sprintf('Дополнительное поле "%s" для модуля "komtet.delivery" не установлено', $key));
+              return false;
+          }
+      }
+      return true;
+    }
+
+    private function optionsValidate($options) {
+      foreach (array('key', 'secret', 'tax_system') as $key) {
+          if (empty($options[$key])) {
+              error_log(sprintf('Option "%s" for module "komtet.delivery" is required', $key));
+              return false;
+          }
+        return true;
+      }
+    }
+
+    private function userValidate($user) {
+        if (empty($user['PERSONAL_PHONE'])) {
+            error_log(sprintf('У пользователя "%s" не указан номер телефона', $user['ID']));
+            return false;
+        }
+        return true;
     }
 }
