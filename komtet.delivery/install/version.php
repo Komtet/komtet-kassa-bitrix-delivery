99bd24551d6645bb0a75905668709a8b0db37e31
Alexandr Sorokin <asorokin.motmom@gmail.com>
2020-07-17 12:16:30 +0300

    release 2.0.0

--- komtet.delivery/install/version.php
+++ komtet.delivery/install/version.php
@@ -1,6 +1,6 @@
 <?php
 
 $arModuleVersion = array(
-    "VERSION" => "1.2.0",
-    "VERSION_DATE" => "2020-06-26 16:00:00"
+    "VERSION" => "2.0.0",
+    "VERSION_DATE" => "2020-07-17 13:00:00"
 );
