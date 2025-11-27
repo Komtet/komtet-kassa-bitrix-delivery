085d6792c85ca89e2408e82927293beb8090b52d
Artem Bychkov <zeleng_han@mail.ru>
2025-11-27 18:11:11 +0300

    release 2.6.0

--- komtet.delivery/install/version.php
+++ komtet.delivery/install/version.php
@@ -1,6 +1,6 @@
 <?php
 
 $arModuleVersion = array(
-    "VERSION" => "2.5.0",
-    "VERSION_DATE" => "2025-02-19 09:00:00"
+    "VERSION" => "2.6.0",
+    "VERSION_DATE" => "2025-11-27 09:00:00"
 );
