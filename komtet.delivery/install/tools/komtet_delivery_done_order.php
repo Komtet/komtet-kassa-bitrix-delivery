bdc1751f16976aaa140270966178609fe6df53ac
Artem Bychkov <zeleng_han@mail.ru>
2026-08-10 13:14:50 +0300

    В обработку отчета КомтетКассы добавлены проверка подписи и аудит запроса, ограничен метод запроса

--- komtet.delivery/install/tools/komtet_delivery_done_order.php
+++ komtet.delivery/install/tools/komtet_delivery_done_order.php
@@ -5,7 +5,44 @@ define('NO_AGENT_STATISTIC', 'Y');
 define('NOT_CHECK_PERMISSIONS', true);
 
 require $_SERVER['DOCUMENT_ROOT'] . '/bitrix/modules/main/include/prolog_before.php';
-require $_SERVER["DOCUMENT_ROOT"] . "/bitrix/modules/komtet.delivery/include.php";
+require $_SERVER['DOCUMENT_ROOT'] . '/bitrix/modules/komtet.delivery/include.php';
+
+const MODULE_ID = 'komtet.delivery';
+
+if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+    http_response_code(405);
+    exit();
+}
+
+$requestHeaders = getallheaders();
+$requestData = file_get_contents('php://input');
+
+$requestSignature = $requestHeaders['X-HMAC-Signature'] ?? '';
+
+$orderBody = json_decode($requestData, true);
+$callbackUrl = $orderBody['callback_url'];
+
+$secretKey = COption::GetOptionString(MODULE_ID, 'secret_key');
+
+$expectedSignature = hash_hmac('md5', 'POST' . $callbackUrl . $requestData, $secretKey);
+
+$isSignatureValid = hash_equals($expectedSignature, $requestSignature);
+
+CEventLog::Add([
+    'SEVERITY'      => 'INFO',
+    'AUDIT_TYPE_ID' => 'WEBHOOK_DEBUG',
+    'MODULE_ID'     => MODULE_ID,
+    'ITEM_ID'       => $_GET['ORDER_ID'],
+    'DESCRIPTION'   => 'КомтетКасса: аудит отчета о доставке заказа\n' .
+                       ($isSignatureValid ? 'Подпись валидна, обработка заказа' : 'Подпись не валидна, выход') . '\n' .
+                       'Headers: ' . print_r($requestHeaders, true) . '\n' .
+                       'RAW input: ' . $requestData
+]);
+
+if (!$isSignatureValid) {
+    http_response_code(403);
+    exit();
+}
 
 try {
     KomtetDelivery::doneOrder($_GET['ORDER_ID']);
